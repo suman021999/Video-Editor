b278c691bd31efc28353f15c546b98b05639b907
Shank34 <spikbang@gmail.com>
2026-04-17 17:47:01 +0530

    client server

---
+++ client/src/pages/MainPage.vue
@@ -0,0 +1,17 @@
+/// MainPage.vue
+
+<template>
+    <Display/>
+    <Navbar/>
+    <Editor_panel/>
+</template>
+
+<script setup>
+import Display from '../components/Display/Display.vue';
+import Editor_panel from '../components/Editor_panel/Editor_panel.vue';
+import Navbar from '../components/Navbar/Navbar.vue';
+</script>
+
+
+
+
