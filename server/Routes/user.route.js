f1de77c0b79d5f6b7c9e1edf4faf8da11f0eac6d
Shank34 <spikbang@gmail.com>
2026-05-21 12:33:39 +0530

    update: server user.router

--- server/Routes/user.route.js
+++ server/Routes/user.route.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { googleLogin, loginUser, logoutUser, registerUser, } from "../controllers/user.controller.js";
-import { protect } from "../middleware/auth.middleware.js";
+import { pro } from "../middleware/auth.middleware.js";
 
 const router = Router();
 
